5d336becdde419228d588f46d68271e913ea3a86
ivanbard <ivanbardziyan@gmail.com>
2025-05-07 09:49:14 -0400

    styled and structured techgrid

--- src/components/TechGrid.jsx
+++ src/components/TechGrid.jsx
@@ -1,6 +1,18 @@
+//import React from 'react';
 import '../styles/TechGrid.css';
+import { SiPython, SiReact, SiNodedotjs, SiFigma, SiC, SiJavascript, SiDjango } from "react-icons/si";
+import { FaJava } from "react-icons/fa";
 
-const tech = [ 'React', 'Node', 'Python', 'Figma', 'C', 'JavaScript', 'Java', 'Django'];
+const techData = [
+  { name: 'React.js',    icon: <SiReact /> },
+  { name: 'Node.js',     icon: <SiNodedotjs /> },
+  { name: 'Python',      icon: <SiPython /> },
+  { name: 'Figma',      icon: <SiFigma /> },
+  { name: 'C',      icon: <SiC /> },
+  { name: 'JavaScript',      icon: <SiJavascript /> },
+  { name: 'Java',      icon: <FaJava /> },
+  { name: 'Django',      icon: <SiDjango /> },
+];
 
 export default function TechGrid() {
   return (
@@ -8,9 +20,12 @@ export default function TechGrid() {
       <h2>Current Tech</h2>
       <p>I am proficient in many technologies, but I use these the most.</p>
       <div className="tech-grid">
-        {tech.map((t) =>
-          <div key={t} className="tech-card">{t}</div>
-        )}
+        {techData.map((t) => (
+          <div key={t.name} className="tech-card">
+            <div className="tech-icon">{t.icon}</div>
+            <div className="tech-label">{t.name}</div>
+          </div>
+        ))}
       </div>
     </section>
   );
