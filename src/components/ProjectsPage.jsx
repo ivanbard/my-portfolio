2536b65fad06ef0e77fd8763c053121a0acdd01e
ivanbard <ivanbardziyan@gmail.com>
2025-05-09 23:18:03 -0400

    begin creating proper projects page

---
+++ src/components/ProjectsPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+//import '../styles/ProjectsPage.css';
+
+//temp projects to develop styles with
+const allProjects = [
+    {
+      title: 'Realtime Sudoku',
+      desc: 'A realtime, multiplayer Sudoku game',
+      img: '/images/sudoku.png',
+      link: 'https://github.com/you/realtime-sudoku'
+    },
+    {
+      title: 'duyle.dev',
+      desc: 'A simple portfolio website with micro‑interactions',
+      img: '/images/duyle.png',
+      link: 'https://duyle.dev'
+    },
+  ];
