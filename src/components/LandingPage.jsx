c1a0823dd3253260566544e325034f2837bef95a
ivanbard <ivanbardziyan@gmail.com>
2025-05-13 18:42:45 -0400

    added some basic animations for the home page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,13 +4,27 @@ import TechGrid from '../components/TechGrid';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
+import FadeInSection from './FadeInSection';
+
 export default function LandingPage() {
   return (
     <>
-      <Hero />
-      <TechGrid />
-      <Projects />
-      <Contact />
+      <FadeInSection>
+        <Hero />
+      </FadeInSection>
+
+      <FadeInSection delay={0.5}>
+        <TechGrid />
+      </FadeInSection>
+
+      <FadeInSection delay={1}>
+        <Projects />
+      </FadeInSection>
+
+      <FadeInSection delay={1.5}>
+        <Contact />
+      </FadeInSection>
+
     </>
   );
 }
